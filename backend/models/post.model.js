659aa6b402545a947db913a6f356cc7c3ec89add
Farhad Qaunain <fqqaunain@gmail.com>
2026-02-08 05:28:39 +0530

    add post model for social feed

---
+++ backend/models/post.model.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const postSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      trim: true,
+    },
+
+    image: {
+      type: String, 
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    comments: [commentSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+postSchema.pre("save", function (next) {
+  if (!this.text && !this.image) {
+    next(new Error("Post must have text or image"));
+  } else {
+    next();
+  }
+});
+
+module.exports = mongoose.model("Post", postSchema);
