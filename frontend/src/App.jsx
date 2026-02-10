859b5a16cc9673899d320248171e1711a42c94dc
Farhad Qaunain <fqqaunain@gmail.com>
2026-02-11 04:30:59 +0530

    added home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import Login from "./pages/Login";
 import Signup from "./pages/signup";
 import Feed from "./pages/Feed";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
