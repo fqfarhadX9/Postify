50f534f0cd86de3d3777c58db1a2a7ed99a37863
Farhad Qaunain <fqqaunain@gmail.com>
2026-02-12 01:24:14 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://postify-jp7y.onrender.com"
+      "https://postify-lbk3.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
